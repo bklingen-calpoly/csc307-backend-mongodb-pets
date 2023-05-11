95a0743db960d91df3698c173c07c018235e2314
BJ Klingenberg <bklingen@calpoly.edu>
2023-05-11 08:17:30 -0700

    working on add pet to house

--- models/pet-services.js
+++ models/pet-services.js
@@ -1,20 +1,20 @@
 const petModel = require("./pet");
 
-async function getPets(name, job) {
+async function getPets(name, type) {
   let result;
-  if (name === undefined && job === undefined) {
+  if (name === undefined && type === undefined) {
     result = await petModel.find();
-  } else if (name && !job) {
-    result = await findUserByName(name);
-  } else if (job && !name) {
-    result = await findUserByJob(job);
+  } else if (name && !type) {
+    result = await findPetByName(name);
+  } else if (type && !name) {
+    result = await findPetByType(type);
   } else {
-    result = await findUserByNameAndJob(name, job);
+    result = await findPetByNameAndType(name, type);
   }
   return result;
 }
 
-async function findUserById(id) {
+async function findPetById(id) {
   // try {
   return await petModel.findById(id);
   // } catch (error) {
@@ -23,30 +23,30 @@ async function findUserById(id) {
   // }
 }
 
-async function addUser(user) {
+async function addPet(pet) {
   // try {
-  const userToAdd = new petModel(user);
-  const savedUser = await userToAdd.save();
-  return savedUser;
+  const petToAdd = new petModel(pet);
+  const savedPet = await petToAdd.save();
+  return savedPet;
   // } catch (error) {
   //   console.log(error);
   //   return false;
   // }
 }
 
-async function findUserByName(name) {
+async function findPetByName(name) {
   return await petModel.find({ name: name });
 }
 
-async function findUserByJob(job) {
-  return await petModel.find({ job: job });
+async function findPetByType(type) {
+  return await petModel.find({ type: type });
 }
 
-async function findUserByNameAndJob(name, job) {
-  return await petModel.find({ name: name, job: job });
+async function findPetByNameAndType(name, type) {
+  return await petModel.find({ name: name, type: type });
 }
 
-async function deleteUser(id) {
+async function deletePet(id) {
   return await petModel.findByIdAndDelete(id);
 }
 
@@ -56,8 +56,10 @@ async function deleteUser(id) {
 // }
 
 exports.getPets = getPets;
-// exports.findUserById = findUserById;
-// exports.findUserByName = findUserByName;
-// exports.addUser = addUser;
-// exports.deleteUser = deleteUser;
+exports.findPetById = findPetById;
+exports.findPetByName = findPetByName;
+exports.findPetByType = findPetByType;
+exports.findPetByNameAndType = findPetByNameAndType;
+exports.addPet = addPet;
+exports.deletePet = deletePet;
 // exports.disconnectDB = disconnectDB;
